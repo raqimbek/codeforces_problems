7eacc7dafe8db18253fdb94f6099c30598ac76ca
raqimbek <raqimbek@proton.me>
2024-11-04 21:24:54 +0500

    polished to match the codeforces

--- problem_236_a/Problem.java
+++ problem_236_a/Problem.java
@@ -10,18 +10,12 @@ public class Problem {
 
     for (int i = 0; i < input.length(); i++) {
 	if (!inputChars.contains(input.charAt(i))) {
-          System.out.println("set does not contain " + input.charAt(i));
           inputChars.add(input.charAt(i));
           distinctCharNum++;
-          System.out.println("distinct characters number: " + distinctCharNum);
           continue;
         }
-        System.out.println("set does contain " + input.charAt(i));
-        System.out.println("distinct characters number: " + distinctCharNum);
     }
 
-    System.out.println("out of the loop");
-    System.out.println("distinct characters number: " + distinctCharNum);
     if (distinctCharNum%2 != 0) {
       System.out.println("IGNORE HIM!");
     } else {
