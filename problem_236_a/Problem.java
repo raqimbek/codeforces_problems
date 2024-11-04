bcc1ddbae6c1e5e1c2910187e55dfdfce31deabe
raqimbek <raqimbek@proton.me>
2024-11-04 21:16:10 +0500

    bug fixed and renamed vars

--- problem_236_a/Problem.java
+++ problem_236_a/Problem.java
@@ -6,27 +6,26 @@ public class Problem {
     Scanner sc = new Scanner(System.in);
     String input = sc.nextLine();
     HashSet<Character> inputChars = new HashSet<>();
-    int indistinctCharNum = 0;
+    int distinctCharNum = 0;
 
     for (int i = 0; i < input.length(); i++) {
 	if (!inputChars.contains(input.charAt(i))) {
           System.out.println("set does not contain " + input.charAt(i));
           inputChars.add(input.charAt(i));
-          indistinctCharNum++;
-          System.out.println("indistinct characters number: " + indistinctCharNum);
+          distinctCharNum++;
+          System.out.println("distinct characters number: " + distinctCharNum);
           continue;
         }
         System.out.println("set does contain " + input.charAt(i));
-        indistinctCharNum--;
-        System.out.println("indistinct characters number: " + indistinctCharNum);
+        System.out.println("distinct characters number: " + distinctCharNum);
     }
 
     System.out.println("out of the loop");
-    System.out.println("indistinct characters number: " + indistinctCharNum);
-    if (indistinctCharNum%2 != 0) {
-      System.out.println("CHAT WITH HER!");
-    } else {
+    System.out.println("distinct characters number: " + distinctCharNum);
+    if (distinctCharNum%2 != 0) {
       System.out.println("IGNORE HIM!");
+    } else {
+      System.out.println("CHAT WITH HER!");
     }
   }
 }
