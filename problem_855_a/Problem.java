fd29bbe1bf931530e8d9fdd2dd96d887ab823981
raqimbek <raqimbek@proton.me>
2024-11-05 00:09:52 +0500

    polished to match the codeforces criteria

--- problem_855_a/Problem.java
+++ problem_855_a/Problem.java
@@ -3,16 +3,11 @@ import java.util.HashSet;
 
 public class Problem {
   public static void main(String[] a) {
-    System.out.println("How many times the diary has been possessed?");
-
     Scanner sc = new Scanner(System.in);
     int possessionTimes = sc.nextInt();
     sc.nextLine();
-    System.out.println("possesion times: " + possessionTimes);
     HashSet<String> names = new HashSet<>();
     String output = "";
-
-    System.out.println("Please, write the names of the people by the order of their possession of the diary. Write one name per line:");
 
     for (int i = 0; i < possessionTimes; i++) {
       String input = sc.nextLine();
@@ -28,7 +23,6 @@ public class Problem {
       }
     }
 
-    System.out.println("For each possession case I will print YES or NO. If a person possessed the diary more than once, I will print YES. Otherwise I will print NO.");
     System.out.println(output);
   }
 }
