39ed3f91b72d24d2cddcdd660fc7de018dff4f19
raqimbek <raqimbek@proton.me>
2024-11-04 21:38:23 +0500

    removed redundant var distinctCharNum and operations with it

--- problem_236_a/Problem.java
+++ problem_236_a/Problem.java
@@ -6,17 +6,14 @@ public class Problem {
     Scanner sc = new Scanner(System.in);
     String input = sc.nextLine();
     HashSet<Character> inputChars = new HashSet<>();
-    int distinctCharNum = 0;
 
     for (int i = 0; i < input.length(); i++) {
 	if (!inputChars.contains(input.charAt(i))) {
           inputChars.add(input.charAt(i));
-          distinctCharNum++;
-          continue;
         }
     }
 
-    if (distinctCharNum%2 != 0) {
+    if (inputChars.size()%2 != 0) {
       System.out.println("IGNORE HIM!");
     } else {
       System.out.println("CHAT WITH HER!");
