98a6b038c59777b92d06eb01c412123d58452b66
raqimbek <raqimbek@proton.me>
2024-11-05 15:42:35 +0500

    rewrote the whole thing with while loops

--- problem_4_c/Problem.java
+++ problem_4_c/Problem.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class Problem {
   public static void main(String[] a) {
     String output = "";
-    HashSet <String> names = new HashSet<>();
+    ArrayList <String> names = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
     int reqNum = sc.nextInt();
     sc.nextLine();
 
-    for (int i = 0; i < reqNum; i++) {
+    while(reqNum != 0) {
+      reqNum--;
       String input = sc.nextLine();
-      if (names.contains(input)) {
-        int j = 1;
-        while (names.contains(input + j)) {
-          j++;
-        }
-        input += j;
-        output += input + "\n";
-      } else {
+
+      if (!names.contains(input)) {
+        names.add(input);
         output += "OK\n";
+        continue;
       }
 
+      int i = 0;
+      while (names.contains(input + ++i)) {}
+      input += i;
+
       names.add(input);
+      output += input + "\n";
     }
 
     System.out.println(output);
