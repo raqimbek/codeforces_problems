ff9234e3341f24d44efdcb57231180fcff7f95e9
raqimbek <raqimbek@proton.me>
2024-12-12 00:57:03 +0500

    start to refactore using streams api and regex

--- wordcounter/WordCounter.java
+++ wordcounter/WordCounter.java
@@ -6,17 +6,6 @@ public class WordCounter {
     var scanner = new Scanner(System.in);
     var input = scanner.nextLine();
 
-    List<Character> letters = List.of('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-    var letterCounter = new TreeMap<Character, Integer>();
-
-    input.toLowerCase().chars()
-         .mapToObj(c -> (char) c)
-         .filter(c -> letters.contains(c)).toList()
-         .forEach(c -> letterCounter.put(c,
-           Optional.ofNullable(letterCounter.get(c))
-           .orElse(0)+1));
-
-    letterCounter.entrySet()
-      .forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+    
   }
 }
