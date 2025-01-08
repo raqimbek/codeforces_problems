48c11c58f167fbd9807c70e33c0dd5d0f2ebb682
raqimbek <raqimbek@proton.me>
2025-01-08 18:12:15 +0500

    removed debugging logs

--- fizzbuzz/Problem.java
+++ fizzbuzz/Problem.java
@@ -1,21 +1,23 @@
 public class Problem {
   public static void main(String[] args) {
     for (var i = 0; i < 100; i++) {
-      System.out.print("i = ");
-      System.out.println(i);
       if (i == 0) continue;
+
       if (i % 5 == 0 && i % 3 == 0) {
         System.out.println("FizzBuzz");
         continue;
       }
+
       if (i % 3 == 0) {
         System.out.println("Fizz");
         continue;
       }
+
       if (i % 5 == 0) {
         System.out.println("Buzz");
         continue;
       }
+
       System.out.println(i);
     }
   }
