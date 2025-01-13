7b3be38911695a79422fc15efafd5fee99b49e51
raqimbek <raqimbek@proton.me>
2025-01-13 17:05:04 +0500

    first try to solve sudoku

--- sudoku_solver/Problem.java
+++ sudoku_solver/Problem.java
@@ -1,5 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Problem {
   public static void main(String[] args) {
+    var sudokuBoard = new ArrayList(List.of(
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9)),
+                        new ArrayList(List.of(1,2,3,4,5,6,7,8,9))
+    ));
+
     
   }
 }
