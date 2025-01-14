f9bc2ffffd1752674fd7ce7450d4094ed3af9f3a
raqimbek <raqimbek@proton.me>
2025-01-14 18:51:19 +0500

    fixed the bug with if(n < 1 && m > 1000)

--- problem_214_a/Problem.java
+++ problem_214_a/Problem.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -13,16 +12,15 @@ public class Problem {
 
     if (n < 1 || m > 1000) return;
 
-    var pairs = new HashMap<Double, Double>();
     var pairsNum = 0;
 
     for (var b = 0; b <= n && b <= m; b++) {
       var a = Math.sqrt(n - b);
       if (a >= 0) {
-        if (Math.sqrt(m - a) == b &&
-           !pairs.containsKey(a)) pairs.put(a,(double) b);
+        if (Math.sqrt(m - a) == b) pairsNum++;
       }
     }
-    System.out.println(pairs);
+ 
+    System.out.println(pairsNum);
   }
 }
