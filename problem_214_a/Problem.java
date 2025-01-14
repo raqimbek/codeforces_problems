e753e8e4e5c8719500b4eb9ef82f07c616f453b9
raqimbek <raqimbek@proton.me>
2025-01-14 18:44:19 +0500

    unfinished alternative solution

--- problem_214_a/Problem.java
+++ problem_214_a/Problem.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -10,17 +11,18 @@ public class Problem {
     var n = nums.get(0);
     var m = nums.get(1);
 
-    if (n <= 1 || m >= 1000) return;
+    if (n < 1 || m > 1000) return;
 
+    var pairs = new HashMap<Double, Double>();
     var pairsNum = 0;
 
     for (var b = 0; b <= n && b <= m; b++) {
       var a = Math.sqrt(n - b);
       if (a >= 0) {
-        if (Math.sqrt(m - a) == b) pairsNum++;
+        if (Math.sqrt(m - a) == b &&
+           !pairs.containsKey(a)) pairs.put(a,(double) b);
       }
     }
- 
-    System.out.println(pairsNum);
+    System.out.println(pairs);
   }
 }
