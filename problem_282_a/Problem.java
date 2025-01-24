55918d571861c94a8f8d0086736517f5c42809c0
raqimbek <raqimbek@proton.me>
2025-01-24 23:51:02 +0500

    first version of the solution for the problem 282 A

--- problem_282_a/Problem.java
+++ problem_282_a/Problem.java
@@ -3,5 +3,45 @@ import java.util.Scanner;
 public class Problem {
   public static void main(String[] args) {
     var scanner = new Scanner(System.in);
+    var statementNum = Integer.valueOf(scanner.nextLine());
+    if (statementNum < 1 || statementNum > 150) return;
+
+    var value = 0;
+
+    while (statementNum > 0) {
+      statementNum--;
+
+      var statement = scanner.nextLine();
+
+      System.out.println("Debugging...");
+      System.out.print("statement: ");
+      System.out.println(statement);
+      System.out.print("!statement.equals(\"X++\"): ");
+      System.out.println(!statement.equals("X++"));
+      System.out.print("!statement.equals(\"++X\"): ");
+      System.out.println(!statement.equals("++X"));
+      System.out.print("!statement.equals(\"--X\"): ");
+      System.out.println(!statement.equals("--X"));
+      System.out.print("!statement.equals(\"X--\"): ");
+      System.out.println(!statement.equals("X--"));
+
+      if (!statement.equals("X++")
+          && !statement.equals("++X")
+          && !statement.equals("X--")
+          && !statement.equals("--X")
+         ) {
+           System.out.println("wtf?");
+           return;
+         }
+
+      switch(statement) {
+        case "X++":
+        case "++X": value++; break;
+        case "X--":
+        case "--X": value--; break;
+      }
+    }
+
+    System.out.println(value);
   }
 }
