8d05817f7b232268a9316b771ebee1af5f9e4b59
raqimbek <raqimbek@proton.me>
2025-01-30 00:13:34 +0500

    first attempt to solve the problem 50 A

--- problem_50_a/Problem.java
+++ problem_50_a/Problem.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Problem {
   public static void main(String[] args) {
     var scanner = new Scanner(System.in);
+    var inputArr = scanner.nextLine().split(" ");
+    var m = inputArr[0];
+    var n = inputArr[1];
+    System.out.println(Double.valueOf(Math.floor((Integer.valueOf(m)*Integer.valueOf(n))/2)).intValue());
   }
 }
