34928327dd84d30962fb748c320b08d52fed3761
raqimbek <raqimbek@proton.me>
2025-01-30 03:55:54 +0500

    refactored

--- problem_263_a/Problem.java
+++ problem_263_a/Problem.java
@@ -7,17 +7,27 @@ import java.util.HashMap;
 public class Problem {
   public static void main(String[] args) {
     var scanner = new Scanner(System.in);
-    var matrix = new ArrayList<List>();
-    var initCoordinates = new HashMap<Integer, Integer>();
-    for (var i = 0; i < 5; i++) {
-      var row = (new ArrayList<>(Arrays.stream(scanner.nextLine().split(" ")).map(Integer::valueOf).toList()));
-      var initIndex = row.stream()
-         .reduce(0, (a, n) -> {if(n != 1) return ++a; else return a;});
-      if (initIndex < row.size()) initCoordinates.put(i, initIndex);
-      matrix.add(row);
-    }
+    var x = -1;
+    var y = -1;
 
-    System.out.print("coordinates: ");
-    System.out.println(initCoordinates);
+    for (var i = 0; i < 5; i++) {
+      var indexPriorTo1 = Arrays.stream(scanner.nextLine().split(" "))
+                                 .map(Integer::valueOf)
+                                 .reduce(-1, (a, n) -> {
+                                   System.out.println("a = " + a);
+                                   if (n != 1) return ++a;
+                                   return a;
+                                 });
+      System.out.println("indexPriorTo1 = " + indexPriorTo1);
+      if (indexPriorTo1 < 4) {
+        x = i;
+        y = indexPriorTo1 + 1;
+      }
+      System.out.println("y = " + y);
+      System.out.println("x = " + x);
+    }
+    System.out.print(x);
+    System.out.print("=");
+    System.out.println(y);
   }
 }
