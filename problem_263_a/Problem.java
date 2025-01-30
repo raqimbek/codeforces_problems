22a38961dd1738f747bd8fb0ee3ca3f9ed2f82cd
raqimbek <raqimbek@proton.me>
2025-01-30 05:33:13 +0500

    first complete attempt to solve the problem 263 A

--- problem_263_a/Problem.java
+++ problem_263_a/Problem.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Problem {
   public static void main(String[] args) {
@@ -10,31 +8,23 @@ public class Problem {
     var y = -1;
 
     for (var i = 0; i < 5; i++) {
-      var indexBefore1 = Arrays.stream(scanner.nextLine().split(" "))
-                               .map(Integer::valueOf)
-                               .reduce(() -> new HashMap<String, Integer>(Map.of("isOneOccurred", 0, "indexBefore1", -1)), (a, n) -> {
-                                 System.out.println("current number: " + n);
-                                 System.out.println("accumulater: " + a);
+      var indexBefore1 = -1;
+      var row = Arrays.stream(scanner.nextLine().split(" "))
+                      .map(Integer::valueOf)
+                      .toList();
+      var iterator = row.iterator();
 
-                                 if (a.get("isOneOccurred").equals(1)) return a;
+      while(iterator.hasNext()) {
+        var n = iterator.next();
+        if (n == 1) break;
+        indexBefore1++;
+      }
 
-                                 if (n == 1) {
-                                   a.put("isOneOccurred", 1);
-                                   return a;
-                                 }
-
-                                 if (n != 1) {
-                                   a.put("indexBefore1", ++a.get("indexBefore1"));
-                                   return a;
-                                 }
-                               }, (a1, a2) -> {a1.putAll(a2); return a1;}).get("indexBefore1");
-      if (indexBefore1 < 4) {
+      if (indexBefore1 != 4) {
         x = i;
-        System.out.println("index before 1: " + indexBefore1);
         y = indexBefore1 + 1;
       }
     }
-    System.out.println("x = " + x + "; y = " + y);
     System.out.println(Math.abs(x - 2) + Math.abs(y - 2));
   }
 }
