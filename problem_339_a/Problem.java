19f69fb598d9914492088586b76e379c9c59ea64
raqimbek <raqimbek@proton.me>
2025-01-31 05:39:58 +0500

    first attempt to solve the problem 339 A

--- problem_339_a/Problem.java
+++ problem_339_a/Problem.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Problem {
   public static void main(String[] args) {
-    var scanner = new Scanner(System.in);
-    
+    System.out.println(String.join("+", Arrays.stream(new Scanner(System.in).nextLine().split("\\+")).sorted().toList()));
   }
 }
