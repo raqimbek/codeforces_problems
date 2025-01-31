fec9d2f394a4a44bb854789ab1bdaa8f6725bf1d
raqimbek <raqimbek@proton.me>
2025-01-31 06:26:15 +0500

    first attempt to solve the problem 266 A

--- problem_266_a/Problem.java
+++ problem_266_a/Problem.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Problem {
   public static void main(String[] args) {
-    
+    var scanner = new Scanner(System.in);
+    scanner.nextLine();
+    var input = scanner.nextLine().split("");
+    var counter = 0;
+
+    for (var i = 0; i < input.length; i++) {
+      if (i+1 < input.length && input[i].equals(input[i+1])) counter++;
+    }
+    System.out.println(counter);
   }
 }
