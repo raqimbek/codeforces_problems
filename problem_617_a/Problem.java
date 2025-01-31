1882a38ba94fe06fb5f83d34776575b768ca6aea
raqimbek <raqimbek@proton.me>
2025-01-31 13:09:00 +0500

    removed debugging logs

--- problem_617_a/Problem.java
+++ problem_617_a/Problem.java
@@ -9,39 +9,21 @@ public class Problem {
   }
   public static int check(int x) {
     var answer = 0;
-    System.out.println("counter: " + answer);
 
     for (var i = 5; i >= 1; i--) {
-      System.out.println("x = " + x + "; i = " + i);
       if (x > 5 && i == 1) {
-        System.out.print(x + " > 5 && " + i + " == 1: ");
-        System.out.println(x > 5 && i==1);
         x -= 5;
-        System.out.println("5 is subtracted from " + x);
         answer++;
-        System.out.println("the counter is incremented");
-        System.out.println("counter: " + answer);
         answer += check(x);
-        System.out.println("out of the self calling");
-        System.out.println("counter: " + answer);
         break;
       }
 
       if (x % i == 0) {
-        System.out.print(x + " % " + i + " == 0: ");
-        System.out.println(x % i == 0);
-        System.out.println("counter: " + answer);
-        System.out.print(x + " / " + i + " (");
-        System.out.print(x / i);
-        System.out.println(") added to the counter");
         answer += x / i;
-        System.out.println("counter: " + answer);
         break;
       }
     }
 
-    System.out.println("out of the loop");
-    System.out.println("counter: " + answer);
     return answer;
   }
 }
