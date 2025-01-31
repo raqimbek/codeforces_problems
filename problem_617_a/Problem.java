b501584b6424a24b882cfa526f26631341761000
raqimbek <raqimbek@proton.me>
2025-01-31 06:41:27 +0500

    first attempt to solve the problem 617 A

--- problem_617_a/Problem.java
+++ problem_617_a/Problem.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class Problem {
   public static void main(String[] args) {
-    
+    var x = Integer.valueOf(new Scanner(System.in).nextLine());
+    var answer = x % 5 != 0 ? 
+                   x % 4 != 0 ? 
+                     x % 3 != 0 ?
+                       x % 2 != 0 ?
+                         1 : x / 2
+                           : x / 3
+                             : x / 4
+                               : x / 5;
+    System.out.println(answer);
   }
 }
