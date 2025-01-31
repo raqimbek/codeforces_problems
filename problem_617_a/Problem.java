8e02771da3c4fec41a66b21a91bd18e4a2571ac1
raqimbek <raqimbek@proton.me>
2025-01-31 07:08:05 +0500

    the second attempt to solve the problem 617 A

--- problem_617_a/Problem.java
+++ problem_617_a/Problem.java
@@ -3,14 +3,35 @@ import java.util.Scanner;
 public class Problem {
   public static void main(String[] args) {
     var x = Integer.valueOf(new Scanner(System.in).nextLine());
-    var answer = x % 5 != 0 ? 
-                   x % 4 != 0 ? 
-                     x % 3 != 0 ?
-                       x % 2 != 0 ?
-                         1 : x / 2
-                           : x / 3
-                             : x / 4
-                               : x / 5;
+    var answer = check(x);
+    
     System.out.println(answer);
+  }
+  public static int check(int x) {
+    var answer = 0;
+    if (x % 5 != 0) {
+      if (x % 4 != 0) {
+        if (x % 3 != 0) {
+          if (x % 2 != 0) {
+            if (x != 1) {
+              answer++;
+              x -= 5;
+              answer += check(x);
+            } else {
+              answer = 1;
+            }
+          } else {
+            answer = x / 2;
+          }
+        } else {
+          answer = x / 3;
+        }
+      } else {
+        answer = x / 4;
+      }
+    } else {
+     answer = x / 5;
+    }
+    return answer;
   }
 }
