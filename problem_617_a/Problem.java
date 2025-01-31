28f27754092dd59ba91f17dfa1ad10fde92f455c
raqimbek <raqimbek@proton.me>
2025-01-31 13:22:13 +0500

    third attempt to solve the problem 617 A

--- problem_617_a/Problem.java
+++ problem_617_a/Problem.java
@@ -7,21 +7,15 @@ public class Problem {
     
     System.out.println(answer);
   }
+
   public static int check(int x) {
     var answer = 0;
 
-    for (var i = 5; i >= 1; i--) {
-      if (x > 5 && i == 1) {
-        x -= 5;
-        answer++;
-        answer += check(x);
-        break;
-      }
-
-      if (x % i == 0) {
-        answer += x / i;
-        break;
-      }
+    if (x > 5) {
+      answer += x / 5;
+      answer += check(x % 5);
+    } else {
+      answer++;
     }
 
     return answer;
