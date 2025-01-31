fde42393c3078d9f36c6d02d22807934569cb85b
raqimbek <raqimbek@proton.me>
2025-01-31 13:07:46 +0500

    refactored the second attempt to solve the problem617 A

--- problem_617_a/Problem.java
+++ problem_617_a/Problem.java
@@ -9,29 +9,39 @@ public class Problem {
   }
   public static int check(int x) {
     var answer = 0;
-    if (x % 5 != 0) {
-      if (x % 4 != 0) {
-        if (x % 3 != 0) {
-          if (x % 2 != 0) {
-            if (x != 1) {
-              answer++;
-              x -= 5;
-              answer += check(x);
-            } else {
-              answer = 1;
-            }
-          } else {
-            answer = x / 2;
-          }
-        } else {
-          answer = x / 3;
-        }
-      } else {
-        answer = x / 4;
+    System.out.println("counter: " + answer);
+
+    for (var i = 5; i >= 1; i--) {
+      System.out.println("x = " + x + "; i = " + i);
+      if (x > 5 && i == 1) {
+        System.out.print(x + " > 5 && " + i + " == 1: ");
+        System.out.println(x > 5 && i==1);
+        x -= 5;
+        System.out.println("5 is subtracted from " + x);
+        answer++;
+        System.out.println("the counter is incremented");
+        System.out.println("counter: " + answer);
+        answer += check(x);
+        System.out.println("out of the self calling");
+        System.out.println("counter: " + answer);
+        break;
       }
-    } else {
-     answer = x / 5;
+
+      if (x % i == 0) {
+        System.out.print(x + " % " + i + " == 0: ");
+        System.out.println(x % i == 0);
+        System.out.println("counter: " + answer);
+        System.out.print(x + " / " + i + " (");
+        System.out.print(x / i);
+        System.out.println(") added to the counter");
+        answer += x / i;
+        System.out.println("counter: " + answer);
+        break;
+      }
     }
+
+    System.out.println("out of the loop");
+    System.out.println("counter: " + answer);
     return answer;
   }
 }
