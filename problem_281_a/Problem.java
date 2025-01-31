dbdeb070ed705ee2fea2cda6d24687b4a7427a37
raqimbek <raqimbek@proton.me>
2025-01-31 05:50:33 +0500

    first attempt to solve the problem 281 A

--- problem_281_a/Problem.java
+++ problem_281_a/Problem.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Problem {
   public static void main(String[] args) {
-    System.out.println(new Scanner(System.in).nextLine());
+    var str = new Scanner(System.in).nextLine();
+    System.out.println(new StringBuilder(str.substring(0,1).toUpperCase()).append(str.substring(1)));
   }
 }
